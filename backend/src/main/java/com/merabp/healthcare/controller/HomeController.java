1f185940cac5a50aa0d6dee411561632aca70b20
Shakthirupa gopalakrishnan <727723euci043@skcet.ac.in>
2026-05-17 19:57:06 +0530

    Allow home endpoint

---
+++ backend/src/main/java/com/merabp/healthcare/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.merabp.healthcare.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Mera-BP Backend Running Successfully";
+    }
+}
