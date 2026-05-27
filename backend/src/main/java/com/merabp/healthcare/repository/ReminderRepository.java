0360a86b3592d8ee1bf9871cf888932507b1f308
Shakthirupa gopalakrishnan <727723euci043@skcet.ac.in>
2026-05-27 12:36:47 +0530

    Fixed missing Modifying import

--- backend/src/main/java/com/merabp/healthcare/repository/ReminderRepository.java
+++ backend/src/main/java/com/merabp/healthcare/repository/ReminderRepository.java
@@ -1,5 +1,5 @@
 package com.merabp.healthcare.repository;
-
+import org.springframework.data.jpa.repository.Modifying;
 import com.merabp.healthcare.model.Reminder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
